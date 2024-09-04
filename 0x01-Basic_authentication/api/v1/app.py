a6cbd8a22a6fd05948c87dea7c79e66873f9a4fb
SirJupiter <jupitertoby@gmail.com>
2024-09-04 11:28:48 +0100

    2. Error handler: Forbidden

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -30,7 +30,7 @@ def unauthorized(error) -> str:
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """Handler for not allowed access to a resource"""
-    return jsonify({"error": "forbidden"}), 403
+    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
