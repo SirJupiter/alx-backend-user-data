d6759d699d26619cf897363989089578620085ae
SirJupiter <jupitertoby@gmail.com>
2024-09-06 00:06:49 +0100

    13. Require auth with stars

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 
 from flask import request
 from typing import List, TypeVar
+import re
 
 
 class Auth:
@@ -15,20 +16,18 @@ class Auth:
         path (str): The path to check
         excluded_paths (List[str]): List of paths that do not require
          authentication
-
-        Returns:
-            False
         """
-        if not path or not excluded_paths or len(excluded_paths) == 0:
-            return True
-
-        slashed_path = path
-        if not path.endswith('/'):
-            slashed_path += '/'
-
-        if slashed_path in excluded_paths:
-            return False
-
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
         return True
 
     def authorization_header(self, request=None) -> str:
