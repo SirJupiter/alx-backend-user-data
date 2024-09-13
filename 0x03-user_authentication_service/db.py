386fa8188a53cd510e68055f082a05f0ca9c8f5e
SirJupiter <jupitertoby@gmail.com>
2024-09-13 20:55:21 +0100

    2. Find user

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,63 @@
+"""DB module
+"""
+from sqlalchemy import create_engine, tuple_
+# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+        # self.make_session()
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Add a new user to the database"""
+        new_user = User(email, hashed_password)
+
+        try:
+            self.__session.add(new_user)
+            self.__session.commit()
+            return new_user
+        except Exception:
+            self.__session.rollback()
+        finally:
+            return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Finds a user based on a set of filters.
+        """
+        fields, values = [], []
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(value)
+            else:
+                raise InvalidRequestError()
+        result = self._session.query(User).filter(
+            tuple_(*fields).in_([tuple(values)])
+                ).first()
+        if result is None:
+            raise NoResultFound()
+        return result
